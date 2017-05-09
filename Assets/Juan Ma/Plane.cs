2f9f039374b05787aef5254a8e7141649d467d5b
czulua18 <camilozv18@gmail.com>
2017-05-09 12:36:04 -0500

    Todo El Proyecto

---
+++ Assets/Juan Ma/Plane.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Plane : MonoBehaviour {
+
+	public float errorX = 0.5f, errorY = 0.5f, errorZ = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider collision) {
+		GameObject obj = collision.gameObject;
+		Rigidbody rb = collision.GetComponent <Rigidbody>();
+		Vector3 dir = rb.velocity;//obj.transform.forward;
+		Debug.Log("NEW PLANE");
+		if (dir.x > errorX) {
+			Debug.Log ("Positive X");
+		} else if (dir.x < -errorX) {
+			Debug.Log ("Negative X");
+		}
+
+		if (dir.y > errorY) {
+			Debug.Log ("Positive Y");
+		} else if (dir.y < -errorY) {
+			Debug.Log ("Negative Y");
+		}
+
+		if (dir.z > errorZ) {
+			Debug.Log ("Positive Z");
+		} else if (dir.z < -errorZ){
+			Debug.Log ("Negative Z");
+		}
+		Debug.Log("Direction: " + dir.x + "," + dir.y + "," + dir.z);
+	}
+}
---
+++ Assets/Scripts/mover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("a")){
+			GetComponent<Rigidbody> ().velocity = new Vector3(2f,0f,0f);	
+		}
+
+	}
+}
